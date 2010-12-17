2e7c1f9338da88edcb7acde722371c9b8f3e24ab
Todd Merritt <redsox38@gmail.com>
2010-12-16 20:22:46 -0700

    Update db update script to prompt for current version

--- update_db.sh
+++ update_db.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+echo -n "Enter current pannoptes version: "
+read vers
 echo -n "Enter admin user id: "
 read mysql_user
 echo -n "Enter admin password: "
@@ -7,5 +9,14 @@ stty -echo
 read mysql_password
 stty echo
 
-cat dbinit-3.sql | mysql -u $mysql_user -p $mysql_password panoptes
+case $vers in
+    1.2)
+    1.1)
+    1.0)
+        cat dbinit-3.sql | mysql -u $mysql_user -p $mysql_password panoptes
+        ;;
+    *)
+        echo "Unknown version: $vers"
+        ;;
+esac
 
