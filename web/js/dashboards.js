cbc2371334dde81fab424b795803ef70287730b4
Todd Merritt <redsox38@gmail.com>
2011-01-08 17:55:33 -0700

    Added select box for new dashboard widget selector

--- web/js/dashboards.js
+++ web/js/dashboards.js
@@ -2,9 +2,24 @@
 function addDashboardWidget() {
     // append selector box to end of dashboard
     var node = document.createElement("div");
-    node.innerHTML = "foo";
     node.className = "newWidget";
-    node.id = "new_widget_box";
-    
+    node.id = "new_widget_box";    
     dojo.place(node, "dashboard_tab", "last");
+
+    // append widget selector
+    sb = new dijit.form.FilteringSelect({
+   	    id: 'new_widget_type',
+   	    name: 'new_widget_type',
+    	    store: dashboardWidgetStore,
+	    style: 'width: 125px;',
+    	    searchAttr: 'name',
+	    labelAttr: 'name',
+	    onChange: function(e) {
+		alert(dijit.byId('new_widget_type').get('value'));
+	    },
+	    placeHolder: 'select a widget'
+    	});
+
+    sb.placeAt(node);
+
 }
