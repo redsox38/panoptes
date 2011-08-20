f8bb509b737b546b3979a39e00501e72bc45f837
Todd Merritt <redsox38@gmail.com>
2011-08-19 21:18:49 -0700

    Add compressed preload.js

--- web/index.php
+++ web/index.php
@@ -129,7 +129,7 @@ if (is_null($chart_theme)) {
         dojo.require("dijit.form.TextBox");
         dojo.require("dijit.form.Button");
     </script>
-    <script type="text/javascript" src="js/preload.js"></script>
+    <script type="text/javascript" src="js/_preload.js"></script>
     <script type="text/javascript" src="js/utils.js"></script>
     <script type="text/javascript" src="js/discovery.js"></script>
     <script type="text/javascript" src="js/devices.js"></script>
